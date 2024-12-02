9ca3846f3e713ce8c81f0790ef8d8e48009d989a
suleppp <3164181443@qq.com>
2024-12-02 11:04:02 +0800

    微服务和微服务之间的信息传递

--- item-service/src/main/java/com/hmall/item/ItemApplication.java
+++ item-service/src/main/java/com/hmall/item/ItemApplication.java
@@ -1,10 +1,13 @@
 package com.hmall.item;
 
+import com.hmall.api.config.DefaultFeignConfig;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @MapperScan("com.hmall.item.mapper")
+@EnableFeignClients(basePackages = "com.hmall.api.client",defaultConfiguration = DefaultFeignConfig.class)
 @SpringBootApplication
 public class ItemApplication {
     public static void main(String[] args) {
